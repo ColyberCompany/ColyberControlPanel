03bd6ef95f5790b4eaf5441083cfff1509cf3a8b
Jan Wielgus <jan.wielgus12@gmail.com>
2021-11-20 10:35:48 +0100

    Added first test line chart

--- ColyberControlPanel/app/src/main/java/com/example/colybercontrolpanel/SensorReadings.kt
+++ ColyberControlPanel/app/src/main/java/com/example/colybercontrolpanel/SensorReadings.kt
@@ -1,22 +1,45 @@
 package com.example.colybercontrolpanel
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 class SensorReadings : AppCompatActivity() {
+
+    private lateinit var chart: LineChart
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sensor_readings)
+
+        chart = findViewById<LineChart>(R.id.chart)
+
+
+        // TODO: remove this test values of chart
+        val entries = mutableListOf<Entry>()
+        for (i in 1..10)
+            entries.add(Entry(i.toFloat(), i.toFloat()))
+
+        val dataSet = LineDataSet(entries, "Label")
+        dataSet.color = 0xFF0000
+
+        val lineData = LineData(dataSet)
+        chart.data = lineData
+        chart.invalidate() // refresh
     }
 
     // TODO: remove this onClick
     fun testOnClick(view: View) {
         Log.e("asdf", "qwer")
-        var edittext: EditText
+        val edittext: EditText
         if (view is EditText)
             edittext = view
         else
