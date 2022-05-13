06709750ca1894dc234b85d9faf63af7cfc1fb63
Jan Wielgus <jan.wielgus12@gmail.com>
2022-05-13 13:12:15 +0200

    Another old tests

--- ColyberControlPanel/app/src/test/java/com/example/colybercontrolpanel/PIDTuningListAdapterTest.kt
+++ ColyberControlPanel/app/src/test/java/com/example/colybercontrolpanel/PIDTuningListAdapterTest.kt
@@ -12,6 +12,45 @@ class PIDTuningListAdapterTest {
 
         result = PIDTuningListAdapter.valueToPercent(0f, 0f, 1f)
         assertEquals(0f, result)
+
+        result = PIDTuningListAdapter.valueToPercent(1f, 0f, 1f)
+        assertEquals(1f, result)
+
+        result = PIDTuningListAdapter.valueToPercent(-2f, -2f, -1f)
+        assertEquals(0f, result)
+    }
+
+    @Test
+    fun valueToPercentTest2() {
+        var result = PIDTuningListAdapter.valueToPercent(1f, 0f, 1000f)
+        assertEquals(0.001f, result)
+
+        result = PIDTuningListAdapter.valueToPercent(10f, 0f, 1000f)
+        assertEquals(0.01f, result)
+
+        result = PIDTuningListAdapter.valueToPercent(950f, 900f, 1000f)
+        assertEquals(0.5f, result)
+
+        result = PIDTuningListAdapter.valueToPercent(0f, -100f, 100f)
+        assertEquals(0.5f, result)
+    }
+
+    @Test
+    fun valueToPercentOverrangeTest1() {
+        var result = PIDTuningListAdapter.valueToPercent(200f, 0f, 100f)
+        assertEquals(2f, result)
+
+        result = PIDTuningListAdapter.valueToPercent(300f, 0f, 100f)
+        assertEquals(3f, result)
+
+        result = PIDTuningListAdapter.valueToPercent(25f, 50f, 100f)
+        assertEquals(-0.5f , result)
+
+        result = PIDTuningListAdapter.valueToPercent(0f, 50f, 100f)
+        assertEquals(-1f , result)
+
+        result = PIDTuningListAdapter.valueToPercent(-100f, 50f, 100f)
+        assertEquals(-3f , result)
     }
 
     @Test
